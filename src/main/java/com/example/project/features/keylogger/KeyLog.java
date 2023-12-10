64245176a0fe729befded99146ebe93e4fec56bf
phnguynmkhoi <pnmk0811@gmail.com>
2023-12-10 10:44:18 +0700

    fix keylog

--- src/main/java/com/example/project/features/keylogger/KeyLog.java
+++ src/main/java/com/example/project/features/keylogger/KeyLog.java
@@ -4,23 +4,27 @@ import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 
 public class KeyLog {
-    KeyListener KL;
 
+    KeyListener KL = new KeyListener();
     public void start(){
+        KL.able();
+    }
+
+    public KeyLog(){
         try {
             GlobalScreen.registerNativeHook();
         } catch (NativeHookException e) {
             throw new RuntimeException(e);
         }
-        KL = new KeyListener();
         GlobalScreen.addNativeKeyListener(KL);
     }
     public String end(){
-        try {
-            GlobalScreen.unregisterNativeHook();
-        } catch (NativeHookException e) {
-            throw new RuntimeException(e);
-        }
+        KL.disable();
+//        try {
+//            GlobalScreen.unregisterNativeHook();
+//        } catch (NativeHookException e) {
+//            throw new RuntimeException(e);
+//        }
         return KL.getText();
     }
 }
